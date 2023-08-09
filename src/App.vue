00ef1bff9355e024e1f2ff583e193f4206460d0d
MM <phacharanan_th@cmu.ac.th>
2023-08-09 21:03:39 +0700

    4.1

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,10 @@ const { message } = storeToRefs(store)
 
 <template>
   <header class="max-h-screen leading-normal">
-    <div id="flashMessage" v-if="message">
-      <h4>{{ message }}</h4>
+    <div id="flashMessage" v-if="message" class="animate-yellowfade">
+      <h4 class="text-lg">{{ message }}</h4>
     </div>
-    <nav>
+    <nav class="p-8">
       <RouterLink :to="{ name: 'event-list' }">Home | </RouterLink>
       <RouterLink :to="{ name: 'about' }">About</RouterLink>
     </nav>
@@ -30,32 +30,32 @@ const { message } = storeToRefs(store)
   color: #2c3e50;
 }
 
-nav {
+/* nav {
   padding: 30px;
-}
+} */
 
-nav a {
+/* nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 
 /* header {
   line-height: 1.5;
   max-height: 100vh;
 } */
 
-.logo {
+/* .logo {
   display: block;
   margin: 0 auto 2rem;
 }
 
 h4 {
   font-size: 20px;
-}
+} */
 
 @keyframes yellowfade {
   from {
@@ -67,7 +67,11 @@ h4 {
   }
 }
 
-#flashMessage {
+.animate-yellowfade {
   animation: yellowfade 3s ease-in-out;
 }
+
+/* #flashMessage {
+  animation: yellowfade 3s ease-in-out;
+} */
 </style>
